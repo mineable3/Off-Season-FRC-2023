476dad4e8bc69682072fcee3142c425037022341
Emmet Spaeth <emmet.spaeth@gmail.com>
2023-01-17 17:59:31 -0500

    added turret to robotcontainer and command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,7 +19,7 @@ public final class Constants {
   public static final int rightmotorID1 = 1;
   public static final int rightmotorID2 = 2;
   public static final int turretmotorID = 5;
-  public static final double speedMultiplier = 0.5;
+  public static final double speedMultiplier = .5;
   public static final int pigeonID = 0;
 
   public static class OperatorConstants {
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Turret;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -25,6 +26,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public final static DriveTrain m_DriveTrain = new DriveTrain();
+  public final static Turret m_Turret = new Turret();
   
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
